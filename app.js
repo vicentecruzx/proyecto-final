094c07d8b4b1f238a4572b25bf20430cdf99d8e5
Vicente Cruz <faustovicentecruz@gmail.com>
2025-03-18 12:50:53 -0300

    Resolviendo conflictos

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
+<<<<<<< HEAD
 console.log("ejecutando el sistema")
+=======
+//sadsadsadsa.
+let amigo = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    if (!nombreAmigo) {
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigo.push(nombreAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    renderizarAmigos();
+}
+
+
+function renderizarAmigos(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigo.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigo[i];
+        listaAmigos.appendChild(item);
+        
+    }
+}
+
+function sortearAmigo(){
+    if(amigo.length === 0){
+        alert("no hay amigos para sortear");
+        return;
+        
+        
+        
+    }
+    let amigoSorteado = amigo[Math.floor(Math.random() * amigo.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `el amigo selecionado es: ${amigoSorteado}`;
+
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML = "";
+}
+>>>>>>> 5f157b0 (Subiendo mi proyecto web)
